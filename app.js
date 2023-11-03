1e04c19b14ff61a273a2f4948bb4efa9bf0686e5
Ryan Daniels <ryandaniels230@gmail.com>
2023-11-02 23:58:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {config} from "dotenv";
+import { errorMiddleware } from './middlewares/error.js';
+
+config({
+    path: "./data/config.env"
+})
+
+export const app = express();
+
+// Using Middlewares
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+    res.send("Working");
+})
+
+// Importing Routers here
+import user from "./routes/user.js"
+app.use("/api/v1/user", user)
+
+// Using Error Middleware
+app.use(errorMiddleware);
